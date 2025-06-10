03030b842ab2628f3d4fd45a3f93bd0579fb8bd4
anuj sharma <sanuja206123@gmail.com>
2025-06-11 02:51:37 +0530

    Add files via upload

---
+++ faulti.js
@@ -0,0 +1,23 @@
+let random= math.random()
+let a= prompt("enter  first number")
+let c= prompt("enter second number ")
+let b= prompt("enter operation  ")
+
+let obj={
+    "+": "-",
+    "-":"+",
+    "*":"/",
+    "%":"+",
+
+}
+if (randam>0.01){
+    console.log("the result is ${a} ${c} ${b}")
+    alert("the result  is  ${evel($a} ${c} ${b})}")
+
+}
+else{
+    c = obj(c)
+    alert("the result  is  ${evel($a} ${b} ${C})}")
+
+
+}
